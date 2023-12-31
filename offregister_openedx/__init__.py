a91dbb1875fcc91b2aeb0eed046720c0ad8a08ad
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2023-12-30 23:08:15 -0500

    [setup.py,__init__.py] Standardise install scripts across all off* Python packages; [LICENSE-{APACHE,MIT}] Update copyright year range

--- offregister_openedx/__init__.py
+++ offregister_openedx/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 
 __author__ = "Samuel Marks"
-__version__ = "0.0.10-gamma"
+__version__ = "0.0.10-rc1"
+__description__ = "Open EdX deployment module for Fabric (offregister)"
